8e208aa57e5b0bcb2606fe233b3d8be93783999e
Yoonjiwon <k112751@koreainvestment.com>
2022-08-03 10:35:06 +0900

    Set default layout
    * Added views on view
    * Set layout constraints

--- TableViewDataSource/TodoViewController.swift
+++ TableViewDataSource/TodoViewController.swift
@@ -13,6 +13,7 @@ import RxSwift
 class TodoViewController: UIViewController {
     private lazy var topView: UIView = {
         let view = UIView()
+        view.backgroundColor = .secondarySystemBackground
         return view
     }()
 
@@ -26,15 +27,37 @@ class TodoViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
+        setupViews()
     }
 
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
+
     }
 }
 
 private extension TodoViewController {
     func setupViews() {
-        
+        [
+            topView,
+            todoTableView
+        ]
+            .forEach {
+                view.addSubview($0)
+            }
+
+        let topViewHeight: CGFloat = 50.0
+        topView.snp.makeConstraints {
+            $0.top.equalTo(view.safeAreaLayoutGuide)
+            $0.leading.trailing.equalToSuperview()
+            $0.height.equalTo(topViewHeight)
+        }
+
+        todoTableView.snp.makeConstraints {
+            $0.top.equalTo(topView.snp.bottom)
+            $0.leading.equalToSuperview()
+            $0.trailing.equalToSuperview()
+            $0.bottom.equalToSuperview()
+        }
     }
 }
