f34a9525839738aa6ce8f66cbf3554fb602c8e5c
Yoonjiwon <k112751@koreainvestment.com>
2022-08-03 09:14:21 +0900

    Add Libraries
    * Added the snap kit
    * Added the RxSwift
    * Added the RxDatasource

--- TableViewDataSource/ViewController.swift
+++ TableViewDataSource/ViewController.swift
@@ -6,12 +6,30 @@
 //
 
 import UIKit
+import SnapKit
+import RxCocoa
+import RxSwift
 
 class ViewController: UIViewController {
 
+    private lazy var topView: UIView = {
+        let view = UIView()
+        return view
+    }()
+
+    private lazy var todoTableView: UITableView = {
+        let tableView = UITableView()
+        return tableView
+    }()
+
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.backgroundColor = .systemBackground
     }
-
 }
 
+private extension ViewController {
+    func setupViews() {
+        
+    }
+}
